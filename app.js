1b8df71dc8f8f666c1c74fc7249253c72e1d52bc
Erin Witmer <ecwitmer@gmail.com>
2016-10-17 14:36:04 -0400

    working prev and next buttons

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
+// single state variable
 var YT_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 
+var state = {
+    query: '',
+    prevPage: '',
+    nextPage: ''
+};
+
+// state modification functions
 function getDataFromApi(searchTerm, pageToken, callback) {
   var query = {
     part: 'snippet',
     key: 'AIzaSyB2ky77MkDC6a-gwVmcoFPDtNHrMegbcRA',
     q: searchTerm,
-    pageToken: ''
+    pageToken: pageToken
   }
   $.getJSON(YT_BASE_URL, query, callback);
 }
 
+function updatePageToken(date) {
 
+}
+
+// render functions
 function displayYTSearchData(data) {
-
+  state.nextPage = data.nextPageToken;
+  state.prevPage = data.prevPageToken;
+  console.log(state.prevPage)
+  console.log(state.nextPage)
+  console.log(state.query)
   var resultElement = '';
   if (data.items) {
     data.items.forEach(function(item) {
@@ -27,24 +43,26 @@ function displayYTSearchData(data) {
   $('.js-search-results').html(resultElement);
 }
 
-function getNextResult(searchTerm, pageToken, callback) {
-  var query = {
-    part: 'snippet',
-    key: 'AIzaSyB2ky77MkDC6a-gwVmcoFPDtNHrMegbcRA',
-    q: searchTerm,
-    pageToken: pageToken
-  }
-  $.getJSON(YT_BASE_URL, query, callback);
-}
-
-
-
+// event listeners
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
-    var query = $(this).find('.js-query').val();
+    state.query = $(this).find('.js-query').val();
+    var query = state.query
     getDataFromApi(query, '', displayYTSearchData);
-  });
-}
+    });
+  };
+
+
+    $('.js-previous').on('click',function(){
+      getDataFromApi(state.query, state.prevPage, displayYTSearchData);
+    })
+
+
+
+    $('.js-next').on('click',function(){
+      getDataFromApi(state.query, state.nextPage, displayYTSearchData);
+    })
+
 
 $(function(){watchSubmit();});
