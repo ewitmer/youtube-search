72d3a24e5e2bbdae971cbc1c20540d8d422eb648
Erin Witmer <ecwitmer@gmail.com>
2016-10-16 22:13:55 -0400

    working without progression buttons

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var YT_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 
-function getDataFromApi(searchTerm, callback) {
+function getDataFromApi(searchTerm, pageToken, callback) {
   var query = {
     part: 'snippet',
     key: 'AIzaSyB2ky77MkDC6a-gwVmcoFPDtNHrMegbcRA',
-    q: searchTerm
+    q: searchTerm,
+    pageToken: ''
   }
   $.getJSON(YT_BASE_URL, query, callback);
 }
@@ -15,8 +16,9 @@ function displayYTSearchData(data) {
   var resultElement = '';
   if (data.items) {
     data.items.forEach(function(item) {
-     resultElement += '<img src='+ item.snippet.thumbnails.medium.url +'>';
+     resultElement += '<a href=https://www.youtube.com/watch?v='+item.id.videoId+' target=blank><img src='+ item.snippet.thumbnails.medium.url +'></a><a href=https://www.youtube.com/channel/'+item.snippet.channelId+'>More from '+item.snippet.channelTitle+'</a>';
     });
+
   }
   else {
     resultElement += '<p>No results</p>';
@@ -25,11 +27,23 @@ function displayYTSearchData(data) {
   $('.js-search-results').html(resultElement);
 }
 
+function getNextResult(searchTerm, pageToken, callback) {
+  var query = {
+    part: 'snippet',
+    key: 'AIzaSyB2ky77MkDC6a-gwVmcoFPDtNHrMegbcRA',
+    q: searchTerm,
+    pageToken: pageToken
+  }
+  $.getJSON(YT_BASE_URL, query, callback);
+}
+
+
+
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayYTSearchData);
+    getDataFromApi(query, '', displayYTSearchData);
   });
 }
 
